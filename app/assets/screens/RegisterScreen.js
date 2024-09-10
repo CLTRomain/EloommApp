c109e2bd9f130ee20c0e49bac9c19934cb6e8853
rclatot <romainclatotpro.proton.me>
2024-09-10 12:23:08 +0200

    [add]✅ ajouts features page register

--- app/assets/screens/RegisterScreen.js
+++ app/assets/screens/RegisterScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { getAllDatesInRange } from "react-multi-date-picker";
 import {
   View,
   Text,
@@ -7,72 +8,110 @@ import {
   TouchableOpacity,
   TextInput,
   Button,
-  Show,
+  Alert,
 } from "react-native";
-import DatePicker from "react-native-date-picker";
 
 function RegisterScreen() {
   const [EmailTexts, onChangeEmail] = React.useState("");
   const [MotDePasseTexts, onChangeMDP] = React.useState("");
   const [SecondMotDePasseTexts, onChangeSecondMDP] = React.useState("");
-  const [Date, setDate] = React.useState(new Date());
+
+  const isFormValid = () => {
+    return (
+      EmailTexts.length > 0 &&
+      MotDePasseTexts.length > 0 &&
+      SecondMotDePasseTexts.length > 0
+    );
+  };
+
+  function trytolog() {
+    if (EmailTexts.includes("@") && EmailTexts.includes(".")) {
+      if (MotDePasseTexts == SecondMotDePasseTexts) {
+        //http express.js pour resultat db
+      } else {
+        Alert.alert("", "Les Deux Mots De Passe Sont Différent !");
+      }
+    } else {
+      Alert.alert("", "l'email n'est pas correct");
+    }
+
+    return;
+  }
 
   return (
     <SafeAreaView style={styles.form}>
+      <Text style={styles.type}>Email</Text>
       <TextInput
-        style={styles.input}
+        style={styles.inputText}
         autoCapitalize="none" // Empêche la majuscule automatique
         autoCorrect={false} // Désactive la correction automatique
         onChangeText={onChangeEmail}
         placeholder="Email"
         value={EmailTexts}
       />
-      <Button title="Open" onPress={() => setOpen(true)} />
-      <DatePicker
-        modal
-        open={open}
-        date={date}
-        onConfirm={(date) => {
-          setOpen(false);
-          setDate(date);
-        }}
-        onCancel={() => {
-          setOpen(false);
-        }}
-      />
+      {/* <Button title="Open" onPress={() => setOpen(true)} /> */}
+      <Text style={styles.type}>Mot de Passe</Text>
 
       <TextInput
-        style={styles.input}
+        style={styles.inputText}
+        autoCapitalize="none" // Empêche la majuscule automatique
+        autoCorrect={false} // Désactive la correction automatique
         secureTextEntry={true}
         onChangeText={onChangeMDP}
         value={MotDePasseTexts}
         placeholder="Mot De Passe"
       />
+      <Text style={styles.type}>Confirmer Mot De Passe</Text>
+
+      <TextInput
+        style={styles.inputText}
+        autoCapitalize="none" // Empêche la majuscule automatique
+        autoCorrect={false} // Désactive la correction automatique
+        secureTextEntry={true}
+        onChangeText={onChangeSecondMDP}
+        placeholder="Mot De Passe"
+        value={SecondMotDePasseTexts}
+      />
+
       <TouchableOpacity
-        style={styles.button}
-        //onPress={trytolog} // renvoie vers fonction try to log avec mdp et email
+        style={[
+          styles.button,
+          { backgroundColor: isFormValid() ? "#588B8B" : "#A2AEBB" },
+        ]}
+        onPress={trytolog}
+        disabled={!isFormValid()} // Désactiver le bouton si le formulaire n'est pas valide
       >
-        <Text>Se connecter</Text>
+        <Text style={styles.buttonText}>Se connecter</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
+  inputText: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    borderRadius: "15%",
   },
+  type: {
+    marginLeft: 18,
+    marginBottom: 1,
+    fontSize: 16,
+  },
+
   button: {
-    backgroundColor: "#DDDDDD",
+    backgroundColor: "red",
     padding: 10,
     width: "60%",
     borderRadius: "40%",
     alignItems: "center",
     left: "17%",
     justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
   },
   form: {
     top: "40%",
